b48d07f90a878f77cd1bdcc1841f24cb424c1ee8
Hasitha <hasithadilshanjayakody678@gmail.com>
2026-03-24 13:45:17 +0530

    third commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -62,7 +62,7 @@ export const Banner = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
                 <h1>{`Hi! I'm Hasitha`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>UI/UX designer & aspiring front-end developer from Sri Lanka.
+                  <p>Software Engineering undergraduate from Sri Lanka with a passion for Full Stack Development.
                      I combine design thinking with coding to build engaging web applications.</p>
                   <button onClick={downloadCV}>Download Resume<ArrowDownCircle size={25} /></button>
               </div>}
